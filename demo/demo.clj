daec74ffcb4771a35f1ee9a635ae20a354073652
David Jarvis <david@standardtreasury.com>
2015-02-02 19:03:41 -0800

    [demo] Updating the demo to be deliberately awful

--- demo/demo.clj
+++ demo/demo.clj
@@ -1,23 +1,20 @@
 (when something
-  (something-else))
-
-(with-out-str
- (println "Hello, ")
- (println "world!"))
+    (something-else))
 
 (filter even?
-        (range 1 10))
+  (range 1 10))
 
 (filter
- even?
- (range 1 10))
+  even?
+  (range 1 10))
 
-(or
- ala
- bala
- portokala)
+(let [thing1 "some stuff"
+  thing2 "other stuff"]
+  {:thing1 thing1
+  :thing2 thing2})
 
-(let [x 3
-      y 4]
-  (+ (* x x) (* y y)))
-
+( let [x 3
+    y 4]
+  (+ (* x x
+  )(* y y)
+  ))
